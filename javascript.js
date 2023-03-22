c7ead5e36c51864eb5def2914eef19b554e5c1d4
Jane <61692204+JaneMansell@users.noreply.github.com>
2023-03-22 10:43:02 +0000

    Add function to store operator

--- javascript.js
+++ javascript.js
@@ -36,3 +36,56 @@ console.log(operate('+',3,2));
 console.log(operate('-',3,2));
 console.log(operate('*',3,2));
 console.log(operate('/',3,2));
+
+let currentOperator;
+let newOperator;
+
+function setOperators(operator) {
+    if (currentOperator === undefined) {
+        currentOperator = operator;
+    }
+    else if (newOperator === undefined) {
+        newOperator = operator;
+    }
+    else {
+        currentOperator = newOperator;
+        newOperator = operator;
+    }
+
+}
+
+function operatorListener() {
+    console.log(this.id);
+    switch (this.id) {
+        case 'addButton' :
+            setOperators('+');            
+            break;
+        case 'subtractButton' :
+            setOperators('-');
+            break;
+        case 'multButton' :
+            setOperators('*');
+            break;
+        case 'divideButton' :
+            setOperators('/');
+            break;
+        case 'equalsButton' :
+            setOperators('=');
+            break;   
+
+    }
+    console.log(currentOperator); 
+    console.log(newOperator);   
+}
+
+
+
+/*const numberButtons = document.querySelectorAll('.numberButton');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', numberListener);
+});*/
+
+const operatorButtons = document.querySelectorAll('.operatorButton');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', operatorListener);
+});
